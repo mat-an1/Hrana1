e84cd49c6aa3aeb1908f9078f7284e3f80f871e2
mat-an1 <154517892+mat-an1@users.noreply.github.com>
2024-01-25 11:07:14 +0100

    Add files via upload

---
+++ naslijeđivanje/naslijeđivanje.cs
@@ -0,0 +1,38 @@
+﻿using naslijeđivanje;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace naslijeđivanje
+{
+    class Cat
+    {
+        bool opasna;
+        public bool Opasna { get => opasna; set => opasna = value; }
+        public virtual void Opasnost()
+        {
+        
+        Opasna = false;
+            
+        }
+    }
+class Gepard : Cat
+{
+    public override void Opasnost()
+    {
+        Opasna = true;
+    }
+}
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Gepard g = new Gepard();
+        g.Opasnost();
+        Console.WriteLine(g.Opasna);
+        Console.ReadKey();
+    }
+}
+}
