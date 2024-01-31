5604c79901ea21314820725a1bbcf6eb5be6276c
mat-an1 <154517892+mat-an1@users.noreply.github.com>
2024-01-31 22:44:23 +0100

    Add files via upload

---
+++ Klasa.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vjezbaOOP02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Unesi string: ");
+            string s = Console.ReadLine();
+            if (s.Length > 2)
+            {
+                KlasaB klasab = new KlasaB();
+                Console.WriteLine(klasab.BezPrvogIZadnjeg(s));
+            }
+            else
+            {
+                Console.WriteLine("Krivi unos");
+            }
+            Console.ReadKey();
+        }
+    }
+
+    class KlasaB
+    {
+        public string BezPrvogIZadnjeg(string s)
+        {
+            return s.Substring(1, s.Length - 2);
+        }
+    }
+}
