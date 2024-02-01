e9aaea600d0755ebc02108df2d565a8b3e808a8a
mat-an1 <154517892+mat-an1@users.noreply.github.com>
2024-02-01 10:53:21 +0100

    Add files via upload

---
+++ Biljka.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biljka
+{
+    class Biljka
+    {
+    }
+
+    class Stablo : Biljka
+    {
+        bool otpadajuListovi;
+        public bool OtpadajuListovi { get => otpadajuListovi; set => otpadajuListovi = value; }
+        public Stablo(bool otpadajuListovi)
+        {
+            OtpadajuListovi = otpadajuListovi;
+        }
+    }
+
+    class Cvijet : Biljka
+    {
+
+    }
+
+    class Crnogoricno : Stablo
+    {
+        public Crnogoricno() : base(true)
+        {
+        }
+    }
+
+    class Bjelogoricno : Stablo
+    {
+        public Bjelogoricno() : base(false)
+        {
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Bjelogoricno Hrast = new Bjelogoricno();
+            Crnogoricno Bor = new Crnogoricno();
+            Console.WriteLine("Hrast:{0} , Bor: {1}",
+                Hrast.OtpadajuListovi, Bor.OtpadajuListovi);
+
+            Console.ReadKey();
+        }
+    }
+}
