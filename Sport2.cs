efccdb7bdeabe27be2aae74b7ef3cc8a00048485
mat-an1 <154517892+mat-an1@users.noreply.github.com>
2024-04-11 10:55:23 +0200

    Add files via upload

---
+++ Sport2.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sport2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Sport nogomet = new Sport("Nogomet", true, 42);
+            Sport vaterpolo = new Sport("Vaterpolo", true, 84);
+            Sport curling = new Sport("Curling", true, 168);
+        }
+    }
+
+    class Sport
+    {
+        private string Naziv;
+        private bool IgraLiSeLoptom;
+        private byte BrojIgraca;
+
+        public Sport(string naziv)
+        {
+            Naziv = naziv;
+        }
+        public Sport(string naziv, bool igraLiSeLoptom)
+        {
+            Naziv = naziv;
+            IgraLiSeLoptom = igraLiSeLoptom;
+        }
+        public Sport(string naziv, bool igraLiSeLoptom, byte brojIgraca)
+        {
+            Naziv = naziv;
+            IgraLiSeLoptom = igraLiSeLoptom;
+            BrojIgraca = brojIgraca;
+        }
+    }
+
+
+}
